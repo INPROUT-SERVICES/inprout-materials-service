0789e6316363b995416ef34e22f3be425dc84024
Guilherme Ferrari <guilhermesouzaferrari@gmail.com>
2026-01-29 16:59:44 -0300

    29/01/2026

--- src/main/java/br/com/inproutservices/inprout_materials_service/controllers/SolicitacaoController.java
+++ src/main/java/br/com/inproutservices/inprout_materials_service/controllers/SolicitacaoController.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/materiais/solicitacoes")
-@CrossOrigin(origins = "*")
+@CrossOrigin(originPatterns = "*")
 public class SolicitacaoController {
 
     private final SolicitacaoService service;
